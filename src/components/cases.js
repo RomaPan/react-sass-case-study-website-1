0433eb205db9793030b19a3f3f835f418a6e4015
Raman Pantsialeyeu <romasdk7@gmail.com>
2020-05-29 20:04:24 +0300

    final code uploaded to preserve creators work, hamburger menu is not optimised and needs a rework even in his final version

--- src/components/cases.js
+++ src/components/cases.js
@@ -7,42 +7,42 @@ const caseStudies = [
     id: 1,
     subtitle: "Curology",
     title: "A custom formula for your skin’s unique needs",
-    img: "curology-min",
+    img: "curology-min"
   },
   {
     id: 2,
     subtitle: "Yourspace",
     title: "Open space floor plans for you next venture",
-    img: "yourspace-min",
+    img: "yourspace-min"
   },
   {
     id: 3,
     subtitle: "Lumin",
     title: "For your best look ever",
-    img: "lumin-min",
-  },
+    img: "lumin-min"
+  }
 ];
 
 const Cases = () => {
   return (
-    <section className="cases">
-      <div className="container-fluid">
-        <div className="cases-navigation">
-          <div className="cases-arrow prev disabled">
+    <section className='cases'>
+      <div className='container-fluid'>
+        <div className='cases-navigation'>
+          <div className='cases-arrow prev disabled'>
             <CasesPrev />
           </div>
-          <div className="cases-arrow next">
+          <div className='cases-arrow next'>
             <CasesNext />
           </div>
         </div>
-        <div className="row">
-          {caseStudies.map((caseItem) => (
-            <div className="case" key={caseItem.id}>
-              <div className="case-details">
+        <div className='row'>
+          {caseStudies.map(caseItem => (
+            <div className='case' key={caseItem.id}>
+              <div className='case-details'>
                 <span>{caseItem.subtitle}</span>
                 <h2>{caseItem.title}</h2>
               </div>
-              <div className="case-image">
+              <div className='case-image'>
                 <img
                   src={require(`../assets/${caseItem.img}.png`)}
                   alt={caseItem.title}
